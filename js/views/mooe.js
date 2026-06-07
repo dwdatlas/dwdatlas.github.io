3173d68fc00dc2acb1e9d2bd6307dc031eb6a1d1
JO ANN MARIE CAGARA <joannmariecagara@gmail.com>
2026-06-07 14:27:27 +0800

    Rename button to Add MOOE/Special Funds

--- js/views/mooe.js
+++ js/views/mooe.js
@@ -25,7 +25,7 @@ const MOOEView = {
       <div><h2>Fund Releases</h2><p>Track all ADA disbursements per school</p></div>
       <button class="btn btn-primary" onclick="MOOEView.openForm()">
         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/></svg>
-        Add Downloaded Fund
+        Add MOOE/Special Funds
       </button>
     </div>
 
